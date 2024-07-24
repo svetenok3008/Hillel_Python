ae9e9110e0f1832fd76ff5c7aee4baa06dd61b28
svetenok3008 <sve.3008@gmail.com>
2024-07-24 18:35:53 +0300

    initial commit

---
+++ 01_lesson_03/home_work_03_01.py
@@ -0,0 +1,17 @@
+a = int(input("input the first number: "))
+b = int(input("input the second number: "))
+action = input("input the sign for calculation action: ")
+
+if action == "+":
+    print("a + b =", a + b)
+elif action == "-":
+    print("a - b =", a - b)
+elif action == "*":
+    print("a * b =", a * b)
+elif action == "/":
+    if b == 0:
+        print("The denominator can't be 0. Calculation can't be done")
+    else:
+        print("a / b =", a / b)
+else:
+    print("Please choose one of the following simbols for calculation: +, -, *, /")
