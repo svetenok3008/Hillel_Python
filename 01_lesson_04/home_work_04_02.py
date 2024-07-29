961c4d19c16901c866112f8de89c1779c84aabe5
svetenok3008 <sve.3008@gmail.com>
2024-07-29 15:33:07 +0300

    added solution for a = []

--- 01_lesson_04/home_work_04_02.py
+++ 01_lesson_04/home_work_04_02.py
@@ -1,4 +1,5 @@
 a = [0, 7, 9, 12, -1, 100]
+# a = []
 
 print("The first method:")
 b = 0
@@ -6,11 +7,11 @@ i = 0
 while i < (len(a)):
     b += a[i]
     i += 2
-print(b * a[-1])
+print(b * (a[-1] if len(a) != 0 else 0))
 
 print("The second method:")
 b = 0
 for i in range(len(a)):
     if i % 2 == 0:
         b += a[i]
-print(b * a[-1])
+print(b * (a[-1] if len(a) != 0 else 0))
