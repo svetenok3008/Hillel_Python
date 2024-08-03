fbcfa73e37af30bddc212e9846f013e47bc21b50
svetenok3008 <sve.3008@gmail.com>
2024-08-03 16:36:48 +0300

    initial commit

---
+++ 01_lesson_05/home_work_05_03.py
@@ -0,0 +1,15 @@
+punctuation_symbols = r'''!"#$%&'()*+,-./:;<=>?@[\]_^`{|}~'''
+punctuation_symbols_list = [i for i in punctuation_symbols]
+
+user_variable = input('Input your sentence: ')
+
+hashtag = ''.join([i for i in user_variable if i not in punctuation_symbols_list]).title()
+hashtag = "#" + ''.join([i for i in hashtag if i != " "])
+
+if len(hashtag) > 140:
+    hashtag = hashtag[0:140]
+
+print(f'The hashtag is created: {hashtag}')
+
+
+
