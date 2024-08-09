d535eb82d73ddf6162f603c3e6ecd2e3d0732b54
svetenok3008 <sve.3008@gmail.com>
2024-08-09 17:13:37 +0300

    updated for days 11- 19

--- 01_lesson_06/home_work_06_02.py
+++ 01_lesson_06/home_work_06_02.py
@@ -9,31 +9,11 @@ minutes = str(minutes_tpl[1]).zfill(2)
 hours = str(hours_tpl[1]).zfill(2)
 days = str(hours_tpl[0])
 
-print("First method:")
-
-days_dict = {
-    '1': 'день',
-    '2': 'дні',
-    '3': 'дні',
-    '4': 'дні',
-    '5': 'днів',
-    '6': 'днів',
-    '7': 'днів',
-    '8': 'днів',
-    '9': 'днів',
-    '0': 'днів'
-}
-print(days, days_dict[days[-1]] + ",", hours + ":" + minutes + ":" + seconds)
-
-print()
-
-print("Second method:")
-if days[-1] == '1':
+if days[-1] in ('5', '6', '7', '8', '9', '0') or days[0] == '1':
+    word_days = 'днів'
+elif days[-1] == '1':
     word_days = 'день'
 elif days[-1] in ('2', '3', '4'):
     word_days = 'дні'
-elif days[-1] in ('5', '6', '7', '8', '9', '0'):
-    word_days = 'днів'
-
 
 print(days, word_days + ",", hours + ":" + minutes + ":" + seconds)
