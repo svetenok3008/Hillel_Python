92922d01369312a670d22cfedcd30264ec0496c1
svetenok3008 <sve.3008@gmail.com>
2024-08-11 12:55:17 +0300

    PEP 8 update

--- 01_lesson_07/home_work_07_01.py
+++ 01_lesson_07/home_work_07_01.py
@@ -1,5 +1,5 @@
 def say_hi(name, age):
-  return f'Hi. My name is {name} and I\'m {age} years old'
+    return f'Hi. My name is {name} and I\'m {age} years old'
 
 
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
