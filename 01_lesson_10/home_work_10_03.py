d737323d0d2a8fdfb9d7bb0275b09bbdb5c25d12
svetenok3008 <sve.3008@gmail.com>
2024-08-22 23:56:03 +0200

    initial commit

---
+++ 01_lesson_10/home_work_10_03.py
@@ -0,0 +1,12 @@
+def is_even(digit: int) -> bool:
+    """ Перевірка чи є парним число """
+    if digit % 2 == 0:
+        return True
+    else:
+        return False
+
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
