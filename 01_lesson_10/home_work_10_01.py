5dc8597dbcaf4d566cd48d149c28a25550146bbe
svetenok3008 <sve.3008@gmail.com>
2024-08-22 23:43:11 +0200

    initial commit

---
+++ 01_lesson_10/home_work_10_01.py
@@ -0,0 +1,25 @@
+from inspect import isgenerator
+
+
+def pow(x):
+    return x ** 2
+
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    i = 0
+    while i < end:
+        i += 1
+        yield begin
+        print(begin)
+        begin = pow(begin)
+
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
